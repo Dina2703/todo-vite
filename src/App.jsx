1f1c2148bba7c4b0aa8a83382b990df653753b83
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-08-08 19:09:01 -0400

    JSX structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,31 @@ import "./styles.css";
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <form className="new-item-form">
+        <div className="form-row"></div>
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">ToDo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
 
